017cde3c1400a94dd3d3d11b3878d52efd5131ec
Amelia LI <jkramelia8872@gmail.com>
2023-07-03 14:39:47 -0400

    Michael and Amelia completed stringifyJSON and getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,41 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, element) {
+  // Instruction: Use document.body  , element.childNodes  , and element.classList  in your solution
+  // https://learn-2.galvanize.com/cohorts/3848/blocks/879/content_files/Recursion/exercises/recursion.md
+
+  // Initialize the result array
+  var results = [];
+
+  // Initial the element
+  // element === undefined ? element = element : element = document.body;
+  if (element === undefined) {
+    element = document.body;
+  }
+
+  // Check current element, if includes the classNme
+  if (element.classList !== undefined) {
+    for (var i = 0; i <= element.classList.length - 1; i++) {
+      if (element.classList[i] === className) {
+        results.push(element);
+      }
+    }
+  }
+
+  // base case: If current element has no children, return; otherwise, iterate through the children, check each child
+  if (element.childNodes.length === 0) {
+    return results;
+  }
+  // recursive case:
+  if (element.childNodes.length > 0) {
+    for (var i = 0; i <= element.childNodes.length - 1; i++) {
+
+      results = results.concat(getElementsByClassName(className, element.childNodes[i]));
+    }
+  }
+
+  // return the result
+  return results;
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,43 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // Base cases
+  // If obj is null
+  if (obj === null) {
+    return 'null';
+  }
+  // if obj is number, boolean,
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return obj.toString();
+  }
+  // if obj is string
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  // Recursive cases
+  // if object is array
+  if (Array.isArray(obj)) {
+    var arrayResult = obj.map(function(element) {
+      if (typeof element === 'undefined' || typeof element === 'function') {
+        return 'null';
+      } else {
+        return stringifyJSON(element);
+      }
+    });
+    return '[' + arrayResult.join(',') + ']';
+  }
+
+  // if object is an object
+  if (typeof obj === 'object') {
+    var objectResult = [];
+    for (var key in obj) {
+      if (typeof obj[key] !== 'undefined' && typeof obj[key] !== 'function') {
+        var value = stringifyJSON(obj[key]);
+        objectResult.push('"' + key + '":' + value);
+      }
+    }
+    return '{' + objectResult.join(',') + '}';
+  }
+
 };
